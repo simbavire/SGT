1c821b20d2017919127b65e7d844d021ef9b9bb0
Vita Reinfelde <vitarei@hotmail.com>
2021-03-15 12:09:17 +0200

    commit

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -26,6 +26,20 @@ namespace Strings
             //int charPos = fullName.IndexOf("z"); // Location of the letter B
             string lName = fullName.Substring(fullName.IndexOf("z")); // Get last name
             Console.WriteLine(lName ); // Print the result
+
+            string userInput;
+            Console.Write("Input the string : ");
+            userInput = Console.ReadLine();
+            int howManyLetters = userInput.Length;
+            Console.Writeline($"Length of the string is : {howManyLetters}");
+            int inputLength = userInput.Length - 1;
+            Console.Write("The characters of the string in reverse are:\n");
+            while (inputLength >= 0)
+            {
+            Console.Write("{0}", userInput[inputLength-1]);
+            inputLength --;
+            }
+
         }
     }
 }
