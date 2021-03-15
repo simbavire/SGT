5234713d0dbc57faa4282d3fd7c99fc77fb8f69a
Vita Reinfelde <vitarei@hotmail.com>
2021-03-15 21:39:09 +0200

    commit

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -6,15 +6,15 @@ namespace Strings
     {
         static void Main(string[] args)
         {
-            string txt = "Raivis Reinfelds";
+            /* string txt = "Raivis Reinfelds";
             Console.WriteLine(txt.ToUpper());   
             Console.WriteLine(txt.ToLower()); 
             string firstName = "Janis";
-            string lastName = "Berzins";
+            string lastName = "Berzins"; */
 
 //string name = firstName + " " +  lastName;
 //Console.WriteLine(name);
-            string name = string.Concat(firstName,  lastName);
+            /* string name = string.Concat(firstName,  lastName);
             Console.WriteLine(name); // no space between them
             string myString = "Hello there!";
             Console.Write(myString[0]); // Outputs "H"
@@ -31,15 +31,43 @@ namespace Strings
             Console.Write("Input the string : ");
             userInput = Console.ReadLine();
             int howManyLetters = userInput.Length;
-            Console.Writeline($"Length of the string is : {howManyLetters}");
+            Console.WriteLine($"Length of the string is : {howManyLetters}");
             int inputLength = userInput.Length - 1;
             Console.Write("The characters of the string in reverse are:\n");
             while (inputLength >= 0)
             {
-            Console.Write("{0}", userInput[inputLength-1]);
+            Console.Write("{0}", userInput[inputLength]);
             inputLength --;
             }
 
+            for (int i = userInput.Length-1; i >= 0; i --){
+                Console.Write ($"{userInput[i]}");
+            }
+            Console.WriteLine();
+            string [] words = userInput.Split (" ");
+            Console.WriteLine(words.Length); */
+
+            string [] words = {"Computer", "Plate", "Chair", "Girl", "Boy", "Cat", "Dog", "Shirt", "Determination"};
+            int startsWithC = 0, endsWithE = 0, hasLength5 = 0; containsTe = 0; 
+            foreach (string item in words)
+            {
+                
+                if (item.StartsWith ("C")){
+                    startsWithC ++;
+
+                if (item.Contains ("te")){
+                    containsTe++;
+                }
+                
+                }
+                
+            }
+            Console.WriteLine($"Starts with C : {startsWithC}");
+
+
+            //ASS3
+
+            string sentence = "Climb mountains not so the world can see you, but so you can see the world";
         }
     }
 }
